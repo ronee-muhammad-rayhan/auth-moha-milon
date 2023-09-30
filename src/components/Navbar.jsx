5979a2ed9b8f3b7307155b20573c66640881d0f5
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-30 11:07:42 +0600

    login and register routes added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                         }
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Moha Milon</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
